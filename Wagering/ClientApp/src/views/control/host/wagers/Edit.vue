4d45c355e8417a9ecbdd2b905227e5660fef7972
Gabe Lopez <glopez11193@gmail.com>
2020-03-21 17:39:00 -0500

    Update Edit.vue

--- Wagering/ClientApp/src/views/control/host/wagers/Edit.vue
+++ Wagering/ClientApp/src/views/control/host/wagers/Edit.vue
@@ -5,21 +5,29 @@
 </template>
 <script>
     export default {
-        props: {
-            //id goes here
-        },
         data() {
             return {
-                users,
-                data
+                id: this.$route.params.id,
+                users: [],
+                data: null
             };
         },
         methods: {
             getUsers() {
                 //for v-autocomplete to add users
+                this.$axios.get("/api/users/" /*+ username*/).then(response => {
+                    if (response.status == 200) {
+                        //set autocomplete data
+                    }
+                });
             },
             getData() {
                 //for wager data
+                this.$axios.get("/api/wagers/host/" + this.id).then(response => {
+                    if (response.status == 200) {
+                        //set data
+                    }
+                });
             }
         },
         created() {
