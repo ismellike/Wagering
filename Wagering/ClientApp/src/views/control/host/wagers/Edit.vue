b9ff64251974a7e29602e2d0c447d7a58413e9a8
Gabe Lopez <glopez11193@gmail.com>
2020-03-23 16:24:43 -0500

    Work on autocomplete

--- Wagering/ClientApp/src/views/control/host/wagers/Edit.vue
+++ Wagering/ClientApp/src/views/control/host/wagers/Edit.vue
@@ -1,6 +1,18 @@
 <template>
     <v-container>
         <!--Wager Settings-->
+        <v-autocomplete v-model="form.select"
+                        :loading="form.loading"
+                        :items="users"
+                        :search-input.sync="search"
+                        cache-items
+                        class="mx-4"
+                        hide-no-data
+                        hide-details
+                        label="Username"
+                        item-text="displayName"
+                        prepend-icon="mdi-account-plus"
+                        return-object></v-autocomplete>
         <!--Member Section-->
         <!--Winning Split-->
     </v-container>
@@ -10,29 +22,51 @@
         data() {
             return {
                 id: this.$route.params.id,
-                users: null,
+                users: [],
                 data: null,
+                loading: true,
+                search: null,
                 form: {
+                    interval: 500,
+                    timer: null,
+                    loading: false,
+                    select: null,
                     username: null
                 }
             };
         },
+        watch: {
+            search(val) {
+                clearTimeout(this.form.timer);
+                if (val && val.trim() && val !== this.form.select && this.form.username != val) {
+                    this.form.username = val;
+                    this.form.loading = true;
+                    this.form.timer = setTimeout(this.getUsers, this.form.interval);
+                }
+                else {
+                    clearTimeout(this.form.timer);
+                    this.form.loading = false;
+                }
+            },
+        },
         methods: {
             getUsers() {
-                //for v-autocomplete to add users
-                this.$axios.get("/api/profile/search/" + form.username).then(response => {
+                this.$axios.get("/api/profile/search/" + this.form.username).then(response => {
                     if (response.status == 200) {
                         this.users = response.data;
                     }
+                    this.form.loading = false;
                 });
             },
             getData() {
                 //for wager data
+                this.loading = true;
                 this.$axios.get("/api/wagers/" + this.id).then(response => {
                     if (response.status == 200) {
                         //set data
                         this.data = response.data;
                     }
+                    this.loading = false;
                 });
             }
         },
