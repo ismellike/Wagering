717ea92a17adb06b00a19cf00e7e4c4a74974d6e
Gabe - Desktop <glopez11193@gmail.com>
2020-05-13 21:02:36 -0500

    Using bulma, changed layout and login-menu

--- Wagering/wwwroot/js/site.js
+++ Wagering/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $(".navbar-burger").click(function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+});
