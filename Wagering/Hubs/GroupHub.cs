8a83d3542b17613cc962bed6b56e7d05a658f73f
Gabe Lopez <glopez11193@gmail.com>
2020-05-18 14:04:10 -0500

    DB storage of signalr

--- Wagering/Hubs/GroupHub.cs
+++ Wagering/Hubs/GroupHub.cs
@@ -3,38 +3,78 @@ using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
 using Wagering.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Wagering.Hubs
 {
     [Authorize]
     public class GroupHub : Hub
     {
-        private readonly static ConnectionMapping<string> _connections =
-            new ConnectionMapping<string>();
-
-        public override Task OnConnectedAsync()
+        private readonly ApplicationDbContext _context;
+        public GroupHub(ApplicationDbContext context)
         {
-            _connections.Add(Context.UserIdentifier, Context.ConnectionId);
-            return base.OnConnectedAsync();
+            _context = context;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var id = Context.UserIdentifier;
+            var user = await _context.Profiles.Where(x => x.Id == id)
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                var connection = user.Connections.FirstOrDefault(x => x.ConnectionID == Context.ConnectionId);
+                if (connection != null)
+                {
+                    connection.Connected = true;
+                }
+                else
+                {
+                    user.Connections.Add(new Connection
+                    {
+                        ConnectionID = Context.ConnectionId,
+                        UserAgent = Context.GetHttpContext().Request.Headers["User-Agent"],
+                        Connected = true
+                    });
+                }
+                _context.SaveChanges();
+            }
+            await base.OnConnectedAsync();
         }
 
         public async Task AddToGroup(string id, string groupName)
         {
-            foreach (var connectionId in _connections.GetConnections(id))
-                await Groups.AddToGroupAsync(connectionId, groupName);
+            var user = await _context.Profiles.AsNoTracking().Where(x => x.Id == id)
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync();
+            if (user != null)
+                foreach (Connection connection in user.Connections)
+                    if (connection.Connected)
+                        await Groups.AddToGroupAsync(connection.ConnectionID, groupName);
         }
 
         public async Task RemoveFromGroup(string id, string groupName)
         {
-            foreach (var connectionId in _connections.GetConnections(id))
-                await Groups.RemoveFromGroupAsync(connectionId, groupName);
+            var user = await _context.Profiles.AsNoTracking().Where(x => x.Id == id)
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync();
+            if (user != null)
+                foreach (Connection connection in user.Connections)
+                    if (connection.Connected)
+                        await Groups.RemoveFromGroupAsync(connection.ConnectionID, groupName);
         }
 
         public async Task AddUsersToGroup(string name, string[] ids, PersonalNotification notification)
         {
-            foreach (string id in ids)
-                foreach (var connectionId in _connections.GetConnections(id))
-                    await Clients.Client(connectionId).SendAsync("ReceiveGroup", name, notification);
+            var users = await _context.Profiles.AsNoTracking().Where(x => ids.Contains(x.Id))
+            .Include(x => x.Connections)
+            .ToListAsync();
+            foreach (var user in users)
+                foreach (var connection in user.Connections)
+                    if (connection.Connected)
+                        await Clients.Client(connection.ConnectionID).SendAsync("ReceiveGroup", name, notification);
         }
 
         public async Task NotifyGroup(string name, PersonalNotification notification)
@@ -42,10 +82,12 @@ namespace Wagering.Hubs
             await Clients.OthersInGroup(name).SendAsync("GetNotification", notification);
         }
 
-        public override Task OnDisconnectedAsync(Exception exception)
+        public override async Task OnDisconnectedAsync(Exception exception)
         {
-            _connections.Remove(Context.UserIdentifier, Context.ConnectionId);
-            return base.OnDisconnectedAsync(exception);
+            var connection = await _context.Connections.FindAsync(Context.ConnectionId);
+            connection.Connected = false;
+            _context.SaveChanges();
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
